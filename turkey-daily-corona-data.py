d1267712c4b5e0df80225b4baebae778760fd4aa
dsaylikoglu <59064659+dsaylikoglu@users.noreply.github.com>
2020-11-06 17:16:25 +0300

    Update turkey-daily-corona-data.py

--- turkey-daily-corona-data.py
+++ turkey-daily-corona-data.py
@@ -9,15 +9,19 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://covid19.saglik.gov.tr/")
 
-driver.title
-
-print(driver.title)
+#print(driver.title)
 
 try:
     data = WebDriverWait(driver, 40).until(
         EC.presence_of_element_located((By.ID, "vaka_sayilari_home"))
     )
+    root = Tk()
+    root.title("Turkey Daily Corona Virus Data")
+    root.geometry("400x595")
+    data_label = Label(root, text=data.text)
+    data_label.pack(pady=10)
     print(data.text)
+    root.mainloop()
 
 finally:
     driver.quit()
